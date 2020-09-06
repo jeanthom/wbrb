df0dedc1875883dfb650618243d43c952af141de
Jean THOMAS <git0@pub.jeanthomas.me>
2020-09-06 20:39:12 +0200

    Initial Commit

---
+++ wbrb/wbrbView.h
@@ -0,0 +1,13 @@
+//
+//  wbrbView.h
+//  wbrb
+//
+//  Created by Jean THOMAS on 06/09/2020.
+//  Copyright © 2020 Jean THOMAS. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface wbrbView : ScreenSaverView
+
+@end
---
+++ wbrb/wbrbView.m
@@ -0,0 +1,52 @@
+//
+//  wbrbView.m
+//  wbrb
+//
+//  Created by Jean THOMAS on 06/09/2020.
+//  Copyright © 2020 Jean THOMAS. All rights reserved.
+//
+
+#import "wbrbView.h"
+
+@implementation wbrbView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
